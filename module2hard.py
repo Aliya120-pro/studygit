a00efc01fbfbc1491f941259b2e82237712d94e5
Aliya120-pro <zintu007@mail.ru>
2024-07-02 22:56:17 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,7 @@
+for n in range(3, 21):
+    print(f'{n} = ', end='')
+    for x1 in range(1, n):
+        for x2 in range(x1 + 1, n):
+            if n % (x1 + x2) == 0:
+                print(f'{x1}{x2}', end='')
+    print()
